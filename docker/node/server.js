588fbf8689fab10a74e89ed85a2ae025bafeec75
Michael Lin <mlin4589@gmail.com>
2016-07-13 18:43:58 -0400

    added bower

--- docker/node/server.js
+++ docker/node/server.js
@@ -3,6 +3,8 @@ var app = express();
 var hbase = require('hbase-rpc-client');
 
 app.use('/static', express.static(__dirname + '/public'));
+//Bower dependencies
+app.use(express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -12,12 +14,17 @@ app.listen(process.env.PORT, function () {
   console.log('App running on ' + process.env.PORT + "!");
 });
 
+//Need to implement timing/wait until the client loads.
+/*
+// HBase
 var client = hbase({
   zookeeperHosts: [
-    process.env.ZOOKEEPER_QUORUM
+    process.env.ZOOKEEPER_HOSTNAME
   ]
 })
+
 client.on("error", function(error) {
   console.log("hbase client error " + error);
 });
+*/
 
