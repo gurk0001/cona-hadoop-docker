2fef52564912dc7247ac83fd5bf1388f9dd54289
Michael Lin <mlin4589@gmail.com>
2016-08-04 01:23:49 -0400

    Disabled R-Studio

--- docker/spark/runspark.sh
+++ docker/spark/runspark.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/lib/rstudio-server/bin/rserver --server-daemonize 0
+# /usr/lib/rstudio-server/bin/rserver --server-daemonize 0
 $SPARK_HOME/sbin/start-master.sh
-
+tail -f /dev/null
